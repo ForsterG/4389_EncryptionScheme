af1d9aced622a707e47feb8a430a54b7bd8567f9
ForsterG <gwf140030@utdallas.edu>
2017-10-28 10:48:03 -0500

    File Handler Updated
    Overhauled the file handler to work correctly

--- EncryptionSchemeMain/src/FileHandler.java
+++ EncryptionSchemeMain/src/FileHandler.java
@@ -1,42 +1,58 @@
-//import java.io.File;
-//import java.util.List;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Paths;
-
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+
 
 public class FileHandler {
 
 	 private String fileName;
 	 private Path filePath;
-	 private byte[] fileBytes;
-	// private List<String> fileLines;
-	public FileHandler()
-	{
+	 private ArrayList<Integer> fileChars=new ArrayList<Integer>();
+	 
+	public FileHandler() {
 	
 	}
 	
-	public void importFile(String passedFile) throws IOException
-	{
-	
+	public void importFile(String passedFile) throws IOException {
+		
 		fileName = passedFile;
 		filePath = Paths.get(fileName);
-		//fileLines = Files.readAllLines(filePath);
-		fileBytes = Files.readAllBytes(filePath);
 		
-		/*for(int x= 0;x<fileBytes.length-1;x++)
+		if(Files.exists(filePath, LinkOption.NOFOLLOW_LINKS)) {
+			if(Files.isReadable(filePath)) {
+				
+				BufferedReader br = new BufferedReader(new FileReader(passedFile));
+				
+				while(br.ready()) {
+					fileChars.add(br.read());
+				}
+				br.close();
+				System.out.println("File Imported");
+			}
+			else {
+				System.out.println(passedFile+" is not readable");
+			}
+		}
+		else {
+			System.out.println(passedFile+" does not exist");
+		}
+		
+		/*for(int x=0;x<fileChars.size();x++)//Debug statement walk the ArrayList
 		{
-			System.out.println(fileBytes[x]);
-		}*/
-		System.out.println("File Imported");
-		
-		
+			System.out.print(fileChars.get(x)+"\t");
+			char y = (char) fileChars.get(x).intValue();
+			System.out.println(y);
+		}//*/
+
 	}
-	public byte[] returnFile()
-	{
-		return fileBytes;
+	
+	public ArrayList<Integer> returnFile() {
+		return fileChars;
 		
 	}
 }
