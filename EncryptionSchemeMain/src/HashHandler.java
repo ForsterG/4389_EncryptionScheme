1c07ac83913e60b79a00f77ef9dedf1621a0419a
Gusbula <s.gistave@gmail.com>
2017-11-17 16:22:57 -0600

    Update HashHandler.java
    ReturnSalt func added

--- EncryptionSchemeMain/src/HashHandler.java
+++ EncryptionSchemeMain/src/HashHandler.java
@@ -2,29 +2,18 @@ import java.security.*;
 import java.lang.StringBuilder;
 
 class HashHandler{
-    /*public static void main(String []args) throws NoSuchAlgorithmException{
-        
+    public static void main(String []args) throws NoSuchAlgorithmException{
+
         String filecontent= "file"; // user entered password;
         SHA256(filecontent);
-        
-    }//main*/
-    
-    // check
-    public HashHandler() {
-    	
-    }
-    
-    public String generateHash(String userPassword) throws NoSuchAlgorithmException {
-    	
-		return SHA256(userPassword);
-    }
-    
-    private String SHA256(String pass) throws NoSuchAlgorithmException{
-        
-        //generates a random sequence Byte to append to the hash.
-        SecureRandom secure = SecureRandom.getInstance("SHA1PRNG");
-        byte salt[]= new byte[16];
-        secure.nextBytes(salt);
+
+    }//main
+
+
+
+    private static String SHA256(String pass) throws NoSuchAlgorithmException{
+
+        byte [] salt= returnSalt(); //returned salt
          String generatedHash=null;
        try{
             MessageDigest mess= MessageDigest.getInstance("SHA-256");
@@ -32,18 +21,41 @@ class HashHandler{
             byte[] mbyte=mess.digest(pass.getBytes());
             StringBuilder sb= new StringBuilder();
             for (int i=0; i<mbyte.length; i++){
-                
+
                 sb.append(Integer.toString((mbyte[i]&0xff)+0x100,16).substring(1));
             }
-            
+
             generatedHash=sb.toString();
-            //System.out.println(generatedHash);//Debug print hash
+            System.out.println(generatedHash);
             }
         catch(NoSuchAlgorithmException e){
-            
+            //
         }
-        
+
         return generatedHash;
         }
-        
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//  Function to return salt.
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+      public static byte[] returnSalt(){
+
+        byte salt[]=null;
+        try{
+        SecureRandom secure = SecureRandom.getInstance("SHA1PRNG");
+        salt= new byte[16];
+        secure.nextBytes(salt);
+      }catch(NoSuchAlgorithmException e){
+        //
+      }
+
+        return salt;
+
+      }
+
+
+
 }//class
+
